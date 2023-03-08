c432a6a5fb5543e8aeee85a4679ca5e2c35cadd5
Mauricio Sandoval <masandoval@uc.cl>
2023-03-07 21:49:00 -0300

    Api de imágenes

--- app/home.py
+++ app/home.py
@@ -2,6 +2,9 @@ import streamlit as st
 import openai as ai
 import time
 
+import replicate
+import os
+
 from PIL import Image
 
 ai.api_key = st.secrets.apikey
@@ -32,10 +35,50 @@ st.markdown(f'<style>{CSS}</style>', unsafe_allow_html=True)
 image = Image.open('data/Captura de Pantalla 2023-03-04 a la(s) 14.40.01.png')
 st.image(image)
 
-def generar_consulta(lugar, pais, emocion, foco):
-    prompt = f'''Genera un post de instagram sobre {lugar}, {pais}
-     que refleje la emoción de {emocion} y con un foco {foco}'''
+
+@st.cache_resource
+def load_model():
+    model = replicate.models.get("salesforce/blip")
+    version = model.versions.get("2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746")
+    return version
+
+os.environ['REPLICATE_API_TOKEN'] = st.secrets.replitkey
+model = load_model()
+
+
+def generate_caption(image, model):
+    inputs = {
+        # Input image
+        'image': image,
+
+        # Choose a task.
+        'task': "image_captioning",
+
+        # Type question for the input image for visual question answering
+        # task.
+        # 'question': ...,
+
+        # Type caption for the input image for image text matching task.
+        # 'caption': ...,
+    }
+
+    # https://replicate.com/salesforce/blip/versions/2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746#output-schema
+    output = model.predict(**inputs)
+    return output
+
+
+
+def generar_consulta(caption, lugar, ciudad, pais, emocion, foco):
+    prompt = f'''
+    Asume que eres un creador de contenido de muchos seguidoes y quieres subir una foto con la siiguiente descripción:
+    {caption}
+    
+    Esta foto fue tomada en {lugar}, {ciudad}, {pais}
+    
+    Crea un copy que refleje la emoción de {emocion} y con un foco {foco}'''
     return prompt
+
+
 
 def generate_gpt3_response(user_text, print_output=False):
     """
@@ -59,33 +102,40 @@ def generate_gpt3_response(user_text, print_output=False):
     # Return the first choice's text
     return completions.choices[0].text
 
-st.markdown('# Bienvenido a Jasper!! (original)')
+
+
+st.markdown('# Bienvenido a InteLLiPost')
 st.markdown('\n #### Comienza a generar las mejores descripciones para tus fotos de instagram')
 
 photo = st.file_uploader('Ingrese su foto')
 
-st.markdown('\n #### Ahora ya queda poco !! \n #### Cuentanos un poco más sobre el contexto de tu foto y lo que quieres transmitir en tu post')
+if photo:
+    st.image(photo)
 
-st.markdown('\n\n ##### Lugar donde tomaste la foto')
-columns =  st.columns(2)
+    st.markdown('\n #### Ahora ya queda poco !! \n #### Cuentanos un poco más sobre el contexto de tu foto y lo que quieres transmitir en tu post')
 
-lugar = columns[0].text_input('Lugar de la foto')
-pais = columns[1].text_input('País')
+    st.markdown('\n\n ##### Lugar donde tomaste la foto')
+    columns =  st.columns(3)
+
+    lugar = columns[0].text_input('Lugar de la foto')
+    ciudad = columns[1].text_input('Ciudad')
+    pais = columns[2].text_input('País')
 
 
-st.markdown('##### Qué te gustaría transmitir? ')
-columns = st.columns(2)
-emocion = columns[0].text_input('Emoción', 'Alegría')
-enfoque = columns[1].text_input('Enfoque', 'Motivacional')  
+    st.markdown('##### Qué te gustaría transmitir? ')
+    columns = st.columns(2)
+    emocion = columns[0].text_input('Emoción', 'Alegría')
+    enfoque = columns[1].text_input('Enfoque', 'Motivacional')  
 
-boton = st.button('Generar Post')
+    boton = st.button('Generar Post')
 
-if boton:
-    prompt = generar_consulta(lugar, pais, emocion, enfoque)
-    post = generate_gpt3_response(prompt)
-    
-    with st.spinner('Ya estamos generando tu post!'):
-        time.sleep(5)
+    if boton:
+        caption = generate_caption(photo,model)
+        prompt = generar_consulta(caption, lugar, ciudad, pais, emocion, enfoque)
+        post = generate_gpt3_response(prompt)
         
-    st.markdown(f'#### {post}')
+        with st.spinner('Ya estamos generando tu post!'):
+            time.sleep(7)
+            
+        post
     
